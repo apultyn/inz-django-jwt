699380a130f61c2883b409056ab9748d926e72de
Andrzej Pultyn <01178581@pw.edu.pl>
2025-07-05 21:59:45 +0200

    Started testing reviews

--- review_app/tests.py
+++ review_app/tests.py
@@ -5,7 +5,7 @@ from rest_framework.test import APITestCase, URLPatternsTestCase
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import Book
+from .models import Book, Review
 from users.models import NewUser
 
 
@@ -133,3 +133,45 @@ class BookITTests(APITestCase, URLPatternsTestCase):
             data={"title": "Dune", "author": "Frank Herbert"},
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class ReviewITTests(APITestCase, URLPatternsTestCase):
+    urlpatterns = [path("api/", include("review_app.urls"))]
+
+    def setUp(self):
+        self.book1 = Book.objects.create(id="1", title="1984", author="George Orwell")
+        self.book2 = Book.objects.create(
+            id="2", title="Brave New World", author="Aldous Huxley"
+        )
+
+        self.user = NewUser.objects.create_user(
+            password="passwd", email="user@example.com"
+        )
+        self.admin = NewUser.objects.create_user(
+            password="passwd", email="admin@example.com"
+        )
+
+        self.group = Group.objects.create(name="book_admin")
+        self.admin.groups.add(self.group)
+        self.admin.save()
+
+        self.review1 = Review.objects.create(
+            id="1", stars="5", comment="Awesome book", author=self.user, book=self.book1
+        )
+
+    def get_jwt_token(self, user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
+    def test_view_reviews(self):
+        response = self.client.get("/api/reviews/", format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_view_review(self):
+        response = self.client.get("/api/reviews/1/", format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.data
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["comment"], "Awesome book")
+        self.assertEqual(data["stars"], 5)
